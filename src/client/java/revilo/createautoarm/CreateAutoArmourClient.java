3adc83a5dbacd4f6f8f5e9a57a357349c99af5cf
Your Name <you@example.com>
2026-02-01 02:15:08 -0500

    Dependencies working

---
+++ src/client/java/revilo/createautoarm/CreateAutoArmourClient.java
@@ -0,0 +1,10 @@
+package revilo.createautoarm;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CreateAutoArmourClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
